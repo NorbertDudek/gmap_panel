1fc0ca60157bd62c90bb8299f17a69d4a800cea4
Norbert Dudek <norbertdudek@gmail.com>
2023-08-29 13:36:30 +0200

    Code cleanup

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 let currentTabId;
-let gmailTabId;
+let gmapTabId;
 let previousTab;
 let previousUnreadMails = 0;
 
@@ -15,7 +15,7 @@ function setButtonIcon(imageURL) {
     }
 };
 
-function createPinnedTab() {
+function createTab() {
     browser.tabs.create(
         {
             url: "https://www.google.pl/maps",
@@ -28,21 +28,21 @@ function createPinnedTab() {
 function handleSearch(gmapTabs) {
     //console.log("currentTabId: " + currentTabId);
     if (gmapTabs.length > 0) {
-        //console.log("there is a gmail tab");
-        gmailTabId = gmapTabs[0].id;
-        if (gmailTabId === currentTabId) {
-            //console.log("I'm in the gmail tab");
+        //console.log("there is a gmap tab");
+        gmapTabId = gmapTabs[0].id;
+        if (gmapTabId === currentTabId) {
+            //console.log("I'm in the gmap tab");
             browser.tabs.update(previousTab, { active: true, });
         } else {
-            //console.log("I'm NOT in the gmail tab");
+            //console.log("I'm NOT in the gmap tab");
             previousTab = currentTabId;
-            browser.tabs.update(gmailTabId, { active: true, });
+            browser.tabs.update(gmapTabId, { active: true, });
         }
         setButtonIcon(gmapTabs[0].favIconUrl);
     } else {
-        //console.log("there is NO gmail tab");
+        //console.log("there is NO gmap tab");
         previousTab = currentTabId;
-        createPinnedTab();
+        createTab();
     }
 };
 
@@ -53,53 +53,4 @@ function handleClick(tab) {
     querying.then(handleSearch, onError);
 };
 
-function setCheckMailTimeOut(checkMailTime) {
-};
-
-function showNotificationPopup(unreadMails) {
-}
-
-function notificationHandler(unreadMails, showNotifications, repetitiveNotifications) {
-    //var soundNotifications = false; //configurable
-    if (showNotifications) {
-        if (repetitiveNotifications) {
-            showNotificationPopup(unreadMails);
-            /*
-            no sound notifications so far
-            if(soundNotifications){
-              console.log("play sound");
-            };
-            */
-        } else {
-            //console.log("previousUnreadMails: " + previousUnreadMails);
-            //console.log("unreadMails: " + unreadMails);
-            if (previousUnreadMails < unreadMails) {
-                showNotificationPopup(unreadMails);
-                /*
-                no sound notifications so far
-                if(soundNotifications){
-                  console.log("play sound");
-                };
-                */
-            };
-        };
-    };
-    previousUnreadMails = unreadMails;
-};
-
-function checkMail(gmapTabs) {
-};
-
-function update(details) {
-    if (details.reason === "install" || details.reason === "update") {
-        browser.runtime.openOptionsPage();
-    }
-};
-
 browser.browserAction.onClicked.addListener(handleClick);
-browser.runtime.onInstalled.addListener(update);
-
-browser.storage.sync.get("checkMailTime").then(function (item) {
-    var checkMailTime = item.checkMailTime || 1;
-    setCheckMailTimeOut(checkMailTime);
-});
